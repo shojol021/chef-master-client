f79d41b8456a4b7ce9144703930a40d52913cb80
shojol021 <shojol021@gmail.com>
2023-05-04 04:33:10 +0600

    chef page updated with banner

--- src/pages/chefs/Chef.jsx
+++ src/pages/chefs/Chef.jsx
@@ -10,14 +10,32 @@ const Chef = () => {
         <Card className="bg-dark text-white">
             <Card.Img src="https://i.postimg.cc/C5yryCpz/chef-banner.jpg" alt="Card image" />
             <Card.ImgOverlay>
-                <Container>
+                <Container className='d-flex'>
                     <img className='w-50 mx-auto d-block rounded' src={chefData.profileImage} alt="" />
-                    <Card.Title className='text-center mt-4 text-warning'>{chefData.chefName}</Card.Title>
-                    <Card.Text className='text-center'>
-                        <span className='fw-bold text-decoration-underline'>About this chef: </span>
-                        {chefData.shortBio}
-                    </Card.Text>
-                    <Card.Text>Last updated 3 mins ago</Card.Text>
+                    <div className='ms-3 d-flex flex-column justify-content-between'>
+                        <div>
+                            <Card.Title className='text-center mt-4 text-warning'>{chefData.chefName}</Card.Title>
+                            <Card.Text className='text-center'>
+                                <span className='fw-bold'>About this chef: </span>
+                                {chefData.shortBio}
+                            </Card.Text>
+                        </div>
+                        <div className='d-flex justify-content-around'>
+                            <div>
+                                <h5>Likes</h5>
+                                <p>{chefData.likes}</p>
+                            </div>
+                            <div>
+                                <h5>Experience</h5>
+                                <p>{chefData.yearsOfExperience}</p>
+                            </div>
+                            <div>
+                                <h5>Total recipes</h5>
+                                <p>{chefData.numRecipes}</p>
+                            </div>
+                        </div>
+                    </div>
+
                 </Container>
             </Card.ImgOverlay>
         </Card>
